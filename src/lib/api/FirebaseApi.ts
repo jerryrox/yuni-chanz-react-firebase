6301d7026eecee016b977818e773821aa2a11f47
Jerry Kim <jerryrox@live.co.kr>
2021-06-02 10:27:02 +1200

    Removed importing firebase modules

--- src/lib/api/FirebaseApi.ts
+++ src/lib/api/FirebaseApi.ts
@@ -1,20 +1,6 @@
-import firebase from "firebase";
 import { ApiResponse, IApi } from "yuni-chanz-react";
 
 export default abstract class FirebaseApi<T = any> implements IApi<T> {
     
-    readonly firestore = firebase.firestore();
-
-    readonly storage = firebase.storage();
-
-    readonly functions = firebase.functions();
-
-    readonly auth = firebase.auth();
-
-    readonly messaging = firebase.messaging();
-
-    readonly analytics = firebase.analytics();
-
-
     abstract request(): Promise<ApiResponse<T>>;
 }
